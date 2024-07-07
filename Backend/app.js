f4c774f58f2727bebd453be078413f5cc255094f
Ankit gupta <ankitgupta.99390@gmail.com>
2024-07-07 15:13:52 +0530

    create admin and user routes

--- Backend/app.js
+++ Backend/app.js
@@ -4,10 +4,11 @@ const app = express();
 require("dotenv").config();
 require("./connection/connection");
 const user = require("./routes/user");
-
+const Books = require("./routes/book")
 app.use(express.json());
 //routes
 app.use("/api/v1",user);
+app.use("/api/v1",Books);
 //creating port
 // const port = process.env.PORT || 3000;  // Make sure there is no trailing comma
 
